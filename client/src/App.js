855fb25c8f3a5ea2fb4414fa843a06201a0ab117
Anshuman-Programmer <anshumantalukdar02@gmail.com>
2022-11-27 18:13:28 +0530

    react-router-dom-added

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,40 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/Home"
+import Register from "./pages/Register"
+import Login from "./pages/Login"
+import Write from "./pages/Write"
+import Single from "./pages/Single"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      app
+      <RouterProvider router={router}/>
     </div>
   );
 }
