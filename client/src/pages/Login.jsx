74f5789482f9de0b00929a336c23a05a05f98dca
Anshuman-Programmer <anshumantalukdar02@gmail.com>
2022-11-27 19:28:42 +0530

    login-page-ui

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import "./styles/_login.scss"
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='auth'>
+      <h1>Login</h1>
+      <form action="">
+        <input type="text" placeholder='username'/>
+        <input type="password" placeholder='password'/>
+        <button>Login</button>
+      </form>
+    </div>
   )
 }
 
